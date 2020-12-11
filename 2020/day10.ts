e33e8e5d880ec7400b8b09f8f671353d3cccf558
Paul Bernhardt <paul@mappedin.ca>
2020-12-10 21:23:03 -0500

    Day 10 part 2

--- 2020/day10.ts
+++ 2020/day10.ts
@@ -3,6 +3,26 @@ import * as _ from "lodash";
 const readline = require("readline");
 const fs = require("fs");
 
+function pathsFromEnd(
+	start: number,
+	set: Set<number>,
+	map: Map<number, number>
+): number {
+	let total = 0;
+	if (set.has(start)) {
+		for (let i = 1; i < 4; i++) {
+			let num = start + i;
+			total += map.get(num) ?? 0;
+		}
+	}
+	map.set(start, total);
+	if (start == 0) {
+		return total;
+	} else {
+		return pathsFromEnd(start - 1, set, map);
+	}
+}
+
 async function main() {
 	const fileStream = fs.createReadStream("./day10.txt");
 	const file = readline.createInterface({ input: fileStream });
@@ -16,7 +36,8 @@ async function main() {
 		set.add(number);
 		max = Math.max(number, max);
 	}
-	numbers.sort((a, b) => a - b);
+	set.add(0);
+	set.add(max);
 
 	let i = 0;
 	let ones = 0;
@@ -35,8 +56,12 @@ async function main() {
 			continue;
 		}
 	}
-	console.log(ones, threes);
 	console.log(ones * threes);
+
+	const map: Map<number, number> = new Map();
+	map.set(max + 3, 1);
+	let total = pathsFromEnd(max, set, map);
+	console.log(total);
 }
 
 main();
