b4b62d08861627ef4c44e507735f93e2bfa8f5b4
Paul Bernhardt <paul@mappedin.ca>
2021-12-02 00:44:15 -0500

    feat: day 2

---
+++ 2021/source/day2.test.ts
@@ -0,0 +1,58 @@
+import { getPositionValue, getPositionValueWithAim, parseMove } from './day2';
+import { readIntoArray } from './utils/fileUtils';
+describe('when solving the day 2 challenge', () => {
+	it('should parse the sample input into directions', async () => {
+		const moves = await readIntoArray('./source/day2.sample.txt', parseMove);
+		expect(moves).toMatchObject([
+			{
+				direction: 'x',
+				distance: 5,
+			},
+			{
+				direction: 'y',
+				distance: 5,
+			},
+			{
+				direction: 'x',
+				distance: 8,
+			},
+			{
+				direction: 'y',
+				distance: -3,
+			},
+			{
+				direction: 'y',
+				distance: 8,
+			},
+			{
+				direction: 'x',
+				distance: 2,
+			},
+		]);
+	});
+	it('should return 150 on the sample input', async () => {
+		const moves = await readIntoArray('./source/day2.sample.txt', parseMove);
+		const pos = getPositionValue(moves);
+		expect(pos).toBe(150);
+	});
+
+	it('should give the answer for the real data', async () => {
+		const moves = await readIntoArray('./source/day2.txt', parseMove);
+		const pos = getPositionValue(moves);
+		expect(pos).toBe(1728414);
+	});
+});
+
+describe('when solving the day 2 b challenge', () => {
+	it('should return 900 on the sample input', async () => {
+		const moves = await readIntoArray('./source/day2.sample.txt', parseMove);
+		const pos = getPositionValueWithAim(moves);
+		expect(pos).toBe(900);
+	});
+
+	it('should give the answer for the real data', async () => {
+		const moves = await readIntoArray('./source/day2.txt', parseMove);
+		const pos = getPositionValueWithAim(moves);
+		expect(pos).toBe(1765720035);
+	});
+});
---
+++ 2021/source/day2.ts
@@ -0,0 +1,54 @@
+import { number } from 'yargs';
+
+enum Direction {
+	x = 'x',
+	y = 'y',
+}
+interface Move {
+	direction: Direction;
+	distance: number;
+}
+
+export function getPositionValue(moves: Move[]): number {
+	let pos = { x: 0, y: 0 };
+
+	for (const move of moves) {
+		pos[move.direction] += move.distance;
+	}
+	return pos.x * pos.y;
+}
+
+export function getPositionValueWithAim(moves: Move[]): number {
+	let pos = { x: 0, y: 0 };
+	let aim = 0;
+	for (const move of moves) {
+		switch (move.direction) {
+			case Direction.y:
+				aim += move.distance;
+				break;
+			case Direction.x:
+				pos.x += move.distance;
+				pos.y += move.distance * aim;
+		}
+	}
+	return pos.x * pos.y;
+}
+
+export function parseMove(line: string): Move {
+	let direction = Direction.x;
+	let distance = 0;
+	const [dir, dist] = line.split(' ');
+	distance = parseInt(dist);
+	switch (dir) {
+		case 'forward':
+			direction = Direction.x;
+			break;
+		case 'up':
+			distance = -distance;
+		case 'down':
+			direction = Direction.y;
+			break;
+	}
+
+	return { direction, distance };
+}
