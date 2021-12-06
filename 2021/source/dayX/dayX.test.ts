d4e295a30e798c4a0241ddc1d99ce97799c40e92
Paul Bernhardt <paul@mappedin.ca>
2021-12-05 22:44:20 -0500

    fix: day X p 1 real test in template, added p2

--- 2021/source/dayX/dayX.test.ts
+++ 2021/source/dayX/dayX.test.ts
@@ -1,9 +1,23 @@
 import { readIntoArray } from '../utils/fileUtils';
 import { solvePartOne, solvePartTwo } from './dayX';
-describe('when solving the day X challenge', () => {
+describe('when solving the day X part one challenge', () => {
 	it('should return 0 on the sample input', async () => {
 		const values = await readIntoArray('./source/dayX/dayX.sample.txt', (x) => x);
 		const result = solvePartOne(values);
+		expect(result).toBe(0);
+	});
+
+	it('should give the answer for the real data', async () => {
+		const values = await readIntoArray('./source/dayX/dayX.txt', (x) => x);
+		const result = solvePartOne(values);
+		expect(result).toBe(0);
+	});
+});
+
+describe('when solving the day X part two challenge', () => {
+	it('should return 0 on the sample input', async () => {
+		const values = await readIntoArray('./source/dayX/dayX.sample.txt', (x) => x);
+		const result = solvePartTwo(values);
 		expect(result).toBe(0);
 	});
 
