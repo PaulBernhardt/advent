c9be9be3e9a8bc97958b8705b28d5a5b7b6d247d
Paul Bernhardt <paul@mappedin.ca>
2021-12-15 16:45:30 -0500

    feat: setupDay downloads puzzle input

--- 2021/setupDay.sh
+++ 2021/setupDay.sh
@@ -7,9 +7,19 @@ do
     esac
 done
 
+if [ -d "source/day$DAY" ]; then
+	echo "Folder for Day $DAY already exists, aborting!"
+	exit 1
+fi
 mkdir source/day$DAY
 
-touch source/day${DAY}/day${DAY}.txt
+
+if [ -f "env.sh" ]; then
+	source env.sh
+	curl -b session=$COOKIE "https://adventofcode.com/2021/day/$DAY/input" > "source/day${DAY}/day${DAY}.txt"
+else
+	touch source/day${DAY}/day${DAY}.txt
+fi
 touch source/day${DAY}/day${DAY}.sample.txt
 
 cat source/dayX/dayX.ts | sed "s/X/$DAY/g ; s/0/$EXAMPLE/g" > source/day${DAY}/day${DAY}.ts
